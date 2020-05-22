bba5a89ed23e57bc0efe907f274f002caa720742
Ziconius <kris@moozle.wtf>
2020-05-22 17:31:38 +0100

    pushing Jekyll

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Deploying C2 Infrastructure to Kube using GKE",
+        "excerpt":"Deploying C2 infrastructure can be slow and tedious as hell, and that why theres now so many tools to automate deployments. In this blog I’m going to discuss the deploying FudgeC2 to GKE along with other requirements, including TLS, and redirectors. The blog will be broken down into 4 stages...","categories": ["Security"],
+        "tags": ["Command and Control","Kuberenetes"],
+        "url": "http://localhost:4000/security/test-page/",
+        "teaser": null
+      }]
